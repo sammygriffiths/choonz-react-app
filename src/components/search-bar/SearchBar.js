10fb05c8848bcf46e4c783b1d32a60eea760f5ac
Sammy Griffiths <samuel.griffiths@skybettingandgaming.com>
2018-05-02 16:45:19 +0100

    Add basic timed auto-submit function for the search form

--- src/components/search-bar/SearchBar.js
+++ src/components/search-bar/SearchBar.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
-import SearchIcon from 'react-icons/lib/fa/search'
-import '../../css/SearchBar.css'
+import SearchIcon from 'react-icons/lib/fa/search';
+import '../../css/SearchBar.css';
 
 class SearchBar extends Component {
+    constructor() {
+        super();
+        this.state = {
+            search: '',
+            submitTimer: null
+        };
+    }
+
+    onChange = (e) => {
+        clearTimeout(this.state.submitTimer);
+        const state = this.state
+        state[e.target.name] = e.target.value;
+        state['submitTimer'] = setTimeout(this.onSubmit, 1000);
+        this.setState(state);
+    }
+
+    onSubmit = (e) => {
+        clearTimeout(this.state.submitTimer);
+        typeof e !== 'undefined' && e.preventDefault();
+        const term = this.state.search;
+        console.log(term);
+    }
+
     render() {
         return (
-            <form className="App-search">
-                <input type="text" placeholder="Search..." />
+            <form className="App-search" onSubmit={this.onSubmit}>
+                <input type="text" placeholder="Search..." name="search" onChange={this.onChange} />
                 <SearchIcon className="App-search-icon" />
             </form>
         );
