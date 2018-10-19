5e954cadb1baa0b232544fb3456e0fd1d9f1cfbc
Sammy Griffiths <samuel.griffiths@skybettingandgaming.com>
2018-10-19 20:07:30 +0100

    Some work on track listing

--- src/components/results/Track.js
+++ src/components/results/Track.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import sonosHelper from '../../helpers/sonos';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlusSquare } from '@fortawesome/free-solid-svg-icons'
 
 class Track extends Component {
     constructor(props) {
         super(props);
         this.state = props.content;
+        this.state.height = props.height;
     }
 
     addToSonos(e) {
@@ -14,9 +17,10 @@ class Track extends Component {
 
     render() {
         return (
-            <li className="track">
-                <h3>{this.state.name}</h3>
-                <a href={process.env.REACT_APP_QUEUE_URL + '/songs'} onClick={this.addToSonos.bind(this)}>Add</a>
+            <li className="track" style={{ height: this.state.height }}>
+                <img src={this.state.album.images[1].url} />
+                <p>{this.state.name}</p>
+                <a href={process.env.REACT_APP_QUEUE_URL + '/songs'} onClick={this.addToSonos.bind(this)}><FontAwesomeIcon icon={faPlusSquare} /></a>
             </li>
         );
     }
--- src/components/results/TrackList.js
+++ src/components/results/TrackList.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
 import Track from './Track';
+import VirtualList from 'react-virtual-list';
 
 class TrackList extends Component {
     render() {
-        let tracks = [];
-        this.props.tracks.items.forEach(track => {
-            tracks.push(<Track key={track.id} content={track} />);
-        });
-        return <ul className="track-list">{tracks}</ul>;
+        const MyList = ({virtual, itemHeight}) => (
+            <ul style={virtual.style} className="track-list">
+                {virtual.items.map(track => (
+                    <Track key={track.id} content={track} height={itemHeight} />
+                ))}
+            </ul>
+        );
+
+        const List = VirtualList()(MyList);
+
+        // let tracks = [];
+        // this.props.tracks.items.forEach(track => {
+        //     tracks.push(<Track key={track.id} content={track} />);
+        // });
+        
+        // return <ul className="track-list">{tracks}</ul>;
+        return <List
+            items={this.props.tracks.items}
+            itemHeight={100}
+        />
     }
 }
 
